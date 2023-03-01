7bcfffa309c17bd3b723d3ad60e920c78ddb8ce3
Dale Willemse <DaleWillemse000@gmail.com>
2023-03-01 14:52:26 +0200

    can see each link, now to save offline

--- webscraper.py
+++ webscraper.py
@@ -1,10 +1,20 @@
+from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.classcentral.com/'
+url = 'https://www.classcentral.com'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
 }
-response = requests.get(url, headers=headers)
 
-with open('example.html', 'w') as f:
-    f.write(response.text)
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+links = soup.find_all('a')
+
+# print each link's URL
+for link in links:
+    href = link.get('href')
+    if href.startswith('http'):
+        print(href)
+    else:
+        print(url + href)
