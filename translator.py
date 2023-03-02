55b66ed984ee40c8239ae8b27b5f0afcabc96474
Dale Willemse <DaleWillemse000@gmail.com>
2023-03-02 17:01:51 +0200

    scraper working. Translator only 1 page at a time.

--- translator.py
+++ translator.py
@@ -1,11 +1,20 @@
 from bs4 import BeautifulSoup
+import translators.server as tss
 
 with open("www_classcentral_default.html", "r", encoding="utf-8") as f:
     html = f.read()
 
 soup = BeautifulSoup(html, "html.parser")
 
+tags = ["h1", "h2", "h3", "h4", "h5", "h6",
+        "p", "title", "span", "a", "div",
+        "section", "strong"]
 
-paragraphs = soup.find_all('p')
-for p in paragraphs:
-    print(p.get_text())
+html_content = soup.find_all(tags)
+for content in html_content:
+    if content.string and not content.string.isspace():
+        translation = tss.google(content.string, to_language='hi')
+        content.string.replace_with(translation)
+
+with open("TranslatedPage.html", "w", encoding="utf-8") as f:
+    f.write(str(soup))
--- webscraper.py
+++ webscraper.py
@@ -1,20 +1,48 @@
+import os
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.classcentral.com'
+url = 'addurlhere'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
+    'add user agent here'
 }
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.content, 'html.parser')
+visited_urls = set()
+urls_to_visit = set([url])
 
-links = soup.find_all('a')
+if not os.path.exists('html_pages'):
+    os.makedirs('html_pages')
 
-# print each link's URL
-for link in links:
-    href = link.get('href')
-    if href.startswith('http'):
-        print(href)
-    else:
-        print(url + href)
+
+def save_html(url, content):
+    path = os.path.join('html_pages', urlparse(
+        url).path.lstrip('/').replace('/', '_') + '.html')
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
+while urls_to_visit:
+    url = urls_to_visit.pop()
+    visited_urls.add(url)
+    print(f"Visiting {url}")
+
+    try:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+    except:
+        continue
+
+    # Save the HTML content to a file
+    save_html(url, str(soup))
+
+    links = soup.find_all('a')
+    for link in links:
+        href = link.get('href')
+        if href and href.startswith('http'):
+            print(href)
+        elif href and not href.startswith('#'):
+            link_url = url + href if href.startswith('/') else href
+            parsed_url = urlparse(link_url)
+            if parsed_url.netloc == urlparse(url).netloc and link_url not in visited_urls:
+                urls_to_visit.add(link_url)
