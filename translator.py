3d5aae8eddd69147b850a76cf7a1596da5ff7148
Dale Willemse <DaleWillemse000@gmail.com>
2023-03-02 19:53:46 +0200

    Slow, but works.

--- translator.py
+++ translator.py
@@ -1,20 +1,30 @@
 from bs4 import BeautifulSoup
 import translators.server as tss
+import os
 
-with open("addHtmlPath", "r", encoding="utf-8") as f:
-    html = f.read()
+htmlPagesPath = "Webpage"
+outputFolder = "TranslatedPages"
 
-soup = BeautifulSoup(html, "html.parser")
+if not os.path.exists(outputFolder):
+    os.makedirs(outputFolder)
 
-tags = ["h1", "h2", "h3", "h4", "h5", "h6",
-        "p", "title", "span", "a", "div",
-        "section", "strong"]
+for file in os.listdir(htmlPagesPath):
+    if file.endswith(".html" and ".htm"):
+        with open(os.path.join(htmlPagesPath, file), "r", encoding="utf-8") as f:
+            html = f.read()
 
-html_content = soup.find_all(tags)
-for content in html_content:
-    if content.string and not content.string.isspace():
-        translation = tss.google(content.string, to_language='hi')
-        content.string.replace_with(translation)
+        soup = BeautifulSoup(html, "html.parser")
 
-with open("TranslatedPage.html", "w", encoding="utf-8") as f:
-    f.write(str(soup))
+        tags = ["h1", "h2", "h3", "h4", "h5", "h6",
+                "p", "title", "span", "a", "div",
+                "section", "strong"]
+
+        html_content = soup.find_all(tags)
+        for content in html_content:
+            if content.string and not content.string.isspace():
+                translation = tss.google(content.string, to_language='hi')
+                content.string.replace_with(translation)
+
+        outputFilePath = os.path.join(outputFolder, file)
+        with open(outputFilePath, "w", encoding="utf-8") as f:
+            f.write(str(soup))
